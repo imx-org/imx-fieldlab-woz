68d78e2f349bf1511f446c0a232bf78ea802bb27
Joost Farla <joost@qonduct.io>
2023-11-22 11:04:58 +0100

    Outway config

--- src/main/java/nl/geostandaarden/imx/fieldlab/woz/source/rest/RestSource.java
+++ src/main/java/nl/geostandaarden/imx/fieldlab/woz/source/rest/RestSource.java
@@ -1,15 +1,11 @@
 package nl.geostandaarden.imx.fieldlab.woz.source.rest;
 
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import java.util.Map;
-import javax.net.ssl.SSLException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import nl.geostandaarden.imx.orchestrate.engine.source.DataRepository;
 import nl.geostandaarden.imx.orchestrate.engine.source.Source;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.SslProvider;
 
 public class RestSource implements Source {
 
@@ -20,20 +16,9 @@ public class RestSource implements Source {
         .baseUrl(options.getBaseUrl())
         .headers(builder -> options.getHeaders()
             .entrySet()
-            .forEach(entry -> builder.add(entry.getKey(), entry.getValue())))
-        .secure(this::configureSslContext);
+            .forEach(entry -> builder.add(entry.getKey(), entry.getValue())));
 
     repository = new RestRepository(client, options.getPaths());
-  }
-
-  private void configureSslContext(SslProvider.SslContextSpec contextSpec) {
-    try {
-      contextSpec.sslContext(SslContextBuilder.forClient()
-          .trustManager(InsecureTrustManagerFactory.INSTANCE)
-          .build());
-    } catch (SSLException e) {
-      throw new RuntimeException(e);
-    }
   }
 
   @Override
